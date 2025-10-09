9c45caf6403f946f73b8bdc9301e3ed3f07b508e
lqazzz <24020010@vnu.edu.vn>
2025-10-09 10:06:48 +0700

    Update MVC Pattern

---
+++ src/main/java/com/Arkanoid/game/Controller/SceneTransitionController.java
@@ -0,0 +1,5 @@
+package org.example.oopgamefull;
+
+public class SceneTransitionController {
+
+}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.oopgamefull {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.oopgamefull to javafx.fxml;
-    exports org.example.oopgamefull;
-}
