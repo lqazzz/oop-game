55d50e3b7e0e69e4e16c365785e5d6c7d01600f8
Memematrix <thelegendbrick678@gmail.com>
2025-11-11 22:04:33 +0700

    Fix path pad again and adjust angle

--- src/main/java/com/Arkanoid/game/Model/Ball.java
+++ src/main/java/com/Arkanoid/game/Model/Ball.java
@@ -180,11 +180,10 @@ public class Ball extends MovableObject {
 
     public void move() {
         double currentSpeed = GameConfig.DEFAULT_SPEED * speedMultiplier;
-
         dx = currentSpeed * Math.cos(Math.toRadians(angle));
         dy = -currentSpeed * Math.sin(Math.toRadians(angle));
-        ballGroup.setLayoutX(ballGroup.getLayoutX() + currentSpeed * Math.cos(Math.toRadians(angle + Math.random() * 2)));
-        ballGroup.setLayoutY(ballGroup.getLayoutY() - currentSpeed * Math.sin(Math.toRadians(angle + Math.random() * 2)));
+        ballGroup.setLayoutX(ballGroup.getLayoutX() + currentSpeed * Math.cos(Math.toRadians(angle + Math.random() * 1)));
+        ballGroup.setLayoutY(ballGroup.getLayoutY() - currentSpeed * Math.sin(Math.toRadians(angle + Math.random() * 1)));
 
         if (trailEffect != null) {
             trailEffect.addTrail(
